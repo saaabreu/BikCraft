e33126e5bf78aa8ebd4f5e39cd6c77065ff69f4d
Sarah Abreu <sarah.abreu2012@gmail.com>
2020-07-29 23:02:37 -0300

    Javascript anime

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 if (window.SimpleSlide) {
 
   new SimpleSlide ({
-      slide: 'quote', // nome do atributo data-slide="principal"
-      time: 5000, // tempo de transição dos slides
+    slide: 'quote', // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
   });
 
   new SimpleSlide ({
-      slide: 'portfolio',
-      time: 5000, 
-      nav: true,
+    slide: 'portfolio',
+    time: 5000, 
+    nav: true,
   });
 
+}
+
+if (window.SimpleAnime) {
+
+  new SimpleAnime ();
 
 }
 
