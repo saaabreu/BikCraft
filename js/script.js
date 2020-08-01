b3ca5561d2b508dc831fd9fe45dde9fdc15fc1b0
Sarah Abreu <sarah.abreu2012@gmail.com>
2020-07-31 21:30:39 -0300

    Formulario de Envio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//https://github.com/origamid/simple-slide//
+
 if (window.SimpleSlide) {
 
   new SimpleSlide ({
@@ -13,9 +15,25 @@ if (window.SimpleSlide) {
 
 }
 
+//https://github.com/origamid/simple-anime//
+
 if (window.SimpleAnime) {
 
   new SimpleAnime ();
 
 }
+
+//https://github.com/origamid/simple-form//
+
+if (window.SimpleForm) {
+
+  new SimpleForm({
+    form: ".formphp", // seletor do formulário
+    button: "#enviar", // seletor do botão
+    erro: "<div id='form-erro'><h2>Erro no Envio!</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>", // mensagem de erro
+    sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+  });
+
+}
+
 
